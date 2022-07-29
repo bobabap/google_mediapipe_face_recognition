1a980015f92b993f048daecf552f6aee5dabc94e
boboabap <didwnsah0427@gmail.com>
2022-07-29 13:02:59 +0900

    -

--- detection.py
+++ detection.py
@@ -10,122 +10,80 @@ import dlib
 import time
 # start = time.time()
 # print("time :", time.time() - start)
-import requests
-from imutils.video import VideoStream
-from imutils.video import FPS
-import imutils
+import urllib.request
+
 import time
 
 model_name='res10_300x300_ssd_iter_140000.caffemodel'
 prototxt_name='deploy.prototxt.txt'
 
-def get_face_embedding(img): # img = 이미지 자체
-    '''얼굴을 인식하고 임베딩 값을 구한다.'''
-    return face_recognition.face_encodings(img)
 
-def crop_embedding(img):
-
-    done_img = ''
-
-    (height, width) = img.shape[:2]
-    model=cv2.dnn.readNetFromCaffe(prototxt_name,model_name)
-    blob=cv2.dnn.blobFromImage(cv2.resize(img,(300,300)),1.0, (300,300),(104.0,177.0,123.0))
-
-    model.setInput(blob)
-
-    detections=model.forward()
-
-    for i in range(0, detections.shape[2]):
-
-        confidence = detections[0, 0, i, 2]
-        min_confidence=0.9
-
-        if confidence > min_confidence:
-
-            box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
-            (startX, startY, endX, endY) = box.astype("int")
-
-            if height > endY and width > endX : #예외처리   
-                done_img = img[startY:endY,startX:endX]
-        else:
-            return get_face_embedding(img)
-
-    embedded = get_face_embedding(done_img)
+def detection(url, profile_img_emb):
     
-    return embedded
-
-
-
-def detection(url):
+    count = 0
+    if count == 20:
+        return
     
-    image_nparray = np.asarray(bytearray(requests.get(url).content), dtype=np.uint8)
-    image = cv2.imdecode(image_nparray, cv2.IMREAD_COLOR)
+    cap = cv2.VideoCapture(0)
     
-    protoPath = "deploy.prototxt.txt"
-    modelPath = "res10_300x300_ssd_iter_140000.caffemodel"
-
-    detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
-
-    profile_img = crop_embedding(image)
-
-    vs = VideoStream(src=0).start()
-
-    fps = FPS().start()
-
+    face = ''
     while True:
-        frame = vs.read()
-        frame = imutils.resize(frame,width=600)
-        (h, w) = frame.shape[:2]
+        ret, frame = cap.read()
         
-        imageBlob = cv2.dnn.blobFromImage(cv2.resize(frame,(300,300)),1.0, (300,300),(104.0,177.0,123.0), swapRB=False, crop=False)
+        (height, width) = frame.shape[:2]
+        model=cv2.dnn.readNetFromCaffe(prototxt_name,model_name)
+        blob=cv2.dnn.blobFromImage(cv2.resize(frame,(300,300)),1.0, (300,300),(104.0,177.0,123.0))
         
-        detector.setInput(imageBlob)
-        detections = detector.forward()
+        model.setInput(blob)
         
-        for i in range(0,detections.shape[2]):
-            confidence = detections[0,0,i,2]
-            
-            if confidence > 0.5:
-                box = detections[0,0,i,3:7] * np.array([w,h,w,h])
+        detections=model.forward()
+        
+        for i in range(0, detections.shape[2]):
+        
+            confidence = detections[0, 0, i, 2]
+            min_confidence=0.5
+                
+            if confidence > min_confidence:
+                
+                box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
                 (startX, startY, endX, endY) = box.astype("int")
                 
-                face = frame[startY:endY, startX:endX]
-                (fH, fW) = face.shape[:2]
-                
-                if fW < 20 or fH < 20:
-                    continue
+                if height > endY and width > endX : #예외처리   
+                    face = frame[startY:endY,startX:endX]
                     
-                faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,(96, 96), (0, 0, 0), swapRB=True, crop=False)
-                
-                y = startY - 10 if startY - 10 > 10 else startY + 10
-                cv2.rectangle(frame, (startX, startY), (endX, endY),
-                    (0, 0, 255), 2)
-
-        fps.update()
         cv2.imshow("Frame", frame)
         
-        try:
-            now_face = get_face_embedding(frame)
-            test = np.linalg.norm(now_face[0] - profile_img[0], ord=2)
-        except:
-            print('fault')
+        now_face = face_recognition.face_encodings(face)
+
+        # 얼굴인식 안되면 continue
+        if now_face == []:
             continue
         
+        test = np.linalg.norm(now_face[0] - profile_img_emb, ord=2)
+            
+        try:
+            test
+        except:
+            return
+        
+    
         if test < 0.31:
             print(test, '통과')
-            break
+            print(url)
+            return
         else:
             print(test, 'X')
+            count = 0
             pass
         
-        
+
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
         
-    fps.stop()
+    cap.release()
     cv2.destroyAllWindows()
-    vs.stop()
+    return
 
 
 def run():
@@ -142,10 +100,37 @@ def run():
             
             faces_url_list.append(line.strip())
     
+    urlopen = []
+    # segmentation fault 방지를 위해 미리 res를 저장하고 사용
     for url in faces_url_list:
-        detection(url)
+        req = urllib.request.Request(url, headers = {"User-Agent" : "Mozilla/5.0"})
+        res = urllib.request.urlopen(req).read() # x11\xd3\xdb\xd3\xf2\xab\
+        urlopen.append(res)
+    
+    # res 이미지로 변환
+    for res in urlopen:
+        image_nparray = np.asarray(bytearray(res), dtype=np.uint8)
+        image = cv2.imdecode(image_nparray, cv2.IMREAD_COLOR)
+        
+        detection(url, face_recognition.face_encodings(image)[0])
+    return
+       
+    
             
             
 if __name__ == '__main__':
     run()
         
+        
+    # datagen = ImageDataGenerator(
+    #     featurewise_center=True,
+    #     featurewise_std_normalization=True,
+    #     rotation_range=20,
+    #     width_shift_range=0.2,
+    #     height_shift_range=0.2,
+    #     horizontal_flip=True)
+
+    # # 특성별 정규화에 필요한 수치를 계산합니다
+    # # (영위상 성분분석 백색화를 적용하는 경우, 표준편차, 평균, 그리고 주성분이 이에 해당합니다)
+    # datagen.fit(profile_list)
+    # datagen.fit(self_list)
